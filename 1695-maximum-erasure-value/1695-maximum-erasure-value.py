7d8552fa6e2c4bba2049cc21497ede9a965e19b1
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-06-13 19:59:41 -0500

    Time: 1674 ms (67.39%), Space: 28 MB (42.40%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -1,6 +1,7 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         #slide window
+        """
         start = 0
         result = 0 
         temp = 0
@@ -20,7 +21,23 @@ class Solution:
                     temp -= sum(nums[start: hashmap[nums[i]]])
                     start = hashmap[nums[i]] + 1
                     hashmap[nums[i]] = i
-
                     
-        return max(result, temp)
+        return max(result, temp) 
+        """
+        seen = set()
+        cur_max = 0
+        l = 0
+        result = 0
+        
+        for i in nums:
+            while i in seen:
+                cur_max -= nums[l]
+                seen.remove(nums[l]) 
+                l += 1
+            cur_max += i
+            seen.add(i)
+            result = max(cur_max, result)
+            
+        return result
+                
         
