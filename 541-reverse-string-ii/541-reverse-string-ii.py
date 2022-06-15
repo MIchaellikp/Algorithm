c06cf3b50de31b9f550afa58e3ba4c11becfaf17
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-06-15 14:19:36 -0500

    Time: 61 ms (24.04%), Space: 14 MB (91.17%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.py
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         #1.0
         a = list(s)
@@ -12,4 +12,15 @@ class Solution:
                 r -= 1
                 
                 
-        return "".join(a)
+        return "".join(a)"""
+
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        # Two pointers. Another is inside the loop.
+        p = 0
+        while p < len(s):
+            p2 = p + k
+            # Written in this could be more pythonic.
+            s = s[:p] + s[p: p2][::-1] + s[p2:]
+            p = p + 2 * k
+        return s
