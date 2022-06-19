44e5d989ea575a95ef6bd60e28d22fa5da94f26c
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-06-18 21:35:52 -0500

    Time: 2629 ms (44.24%), Space: 30.6 MB (30.77%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,4 +1,4 @@
-class MyQueue:
+"""class MyQueue:
     def __init__(self):
         self.queue = []
         
@@ -30,4 +30,25 @@ class Solution:
             myq.push(nums[i])
             result.append(myq.front())
         
+        return result"""
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque()
+        result = []
+        n = len(nums)
+
+        for i in range(k):
+            while q and q[-1] < nums[i]: 
+                q.pop()
+            q.append(nums[i])
+        result.append(q[0])
+
+        for i in range(k, n):
+            if nums[i-k] == q[0]:
+                q.popleft()
+            while q and q[-1] < nums[i]:
+                q.pop()
+            q.append(nums[i])
+            result.append(q[0])
+
         return result
