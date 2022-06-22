2fb32db82cc0695d5f2527632dd9e81ed443c291
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-06-21 21:38:07 -0500

    Time: 641 ms (77.25%), Space: 49.3 MB (88.00%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        md = 0
+        
+        def helper(r):
+            if not r:
+                return 0
+            return min(helper(r.left), helper(r.right)) + 1
+            
+        md = helper(root)
+        return md"""
+    
+    
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+
+        #根节点的深度为1
+        queue_ = [(root,1)]
+        while queue_:
+            cur, depth = queue_.pop(0)
+            
+            if cur.left == None and cur.right == None:
+                return depth
+            #先左子节点，由于左子节点没有孩子，则就是这一层了
+            if cur.left:
+                queue_.append((cur.left,depth + 1))
+            if cur.right:
+                queue_.append((cur.right,depth + 1))
+
+        return 0
