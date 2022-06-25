7dcfa9dfa6d18ad0246cfbc5b6b1f4b40c78ad55
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-06-24 23:15:29 -0500

    Time: 224 ms (73.52%), Space: 15.3 MB (49.84%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def checkPossibility(self, N: List[int]) -> bool:
         err = 0
         for i in range(1, len(N)):
@@ -6,4 +6,21 @@ class Solution:
                 if err or (i > 1 and i < len(N) - 1 and N[i-2] > N[i] and N[i+1] < N[i-1]):
                     return False
                 err = 1
-        return True
+        return True"""
+
+class Solution(object):
+    def checkPossibility(self, nums):
+    # greedy, find i with nums[i-1]>nums[i]
+    # modify nums[i-1] or nums[i], e.g, [3,4,2,3]
+        cnt = 0
+        for i in range(1, len(nums)):
+            if nums[i - 1] > nums[i]:
+                cnt += 1
+                if i < 2 or nums[i - 2] <= nums[i]:
+                    nums[i - 1] = nums[i] # modify nums[i-1]
+                    print(nums)
+                else:
+                    nums[i] = nums[i - 1]
+                    
+                    # modify nums[i]
+        return cnt <= 1
