5708d3583151578e73b150d93aac8d20994b5c95
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-06-27 16:44:17 -0500

    Time: 124 ms (55.27%), Space: 15.6 MB (45.14%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+"""class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root1:
             return root2
@@ -16,4 +16,37 @@ class Solution:
         root1.left = self.mergeTrees(root1.left, root2.left)
         root1.right = self.mergeTrees(root1.right, root2.right)
         
+        return root1"""
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if not root1: 
+            return root2
+        if not root2: 
+            return root1
+
+        queue = deque()
+        queue.append(root1)
+        queue.append(root2)
+
+        while queue: 
+            node1 = queue.popleft()
+            node2 = queue.popleft()
+            # 更新queue
+            # 只有两个节点都有左节点时, 再往queue里面放.
+            if node1.left and node2.left: 
+                queue.append(node1.left)
+                queue.append(node2.left)
+            # 只有两个节点都有右节点时, 再往queue里面放.
+            if node1.right and node2.right: 
+                queue.append(node1.right)
+                queue.append(node2.right)
+
+            # 更新当前节点. 同时改变当前节点的左右孩子. 
+            node1.val += node2.val
+            if not node1.left and node2.left: 
+                node1.left = node2.left
+            if not node1.right and node2.right: 
+                node1.right = node2.right
+
         return root1
