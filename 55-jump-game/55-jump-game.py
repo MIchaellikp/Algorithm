ec7ce841477fce80bdd7e489cf34673679ffae85
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-11 12:43:56 -0500

    Time: 650 ms (64.81%), Space: 15.2 MB (50.69%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def canJump(self, nums: List[int]) -> bool:
         cover = 0 
         if len(nums) <= 1:
@@ -10,4 +10,14 @@ class Solution:
                 return True
             i += 1
             
+        return False"""
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        minIndex = len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if i+nums[i]>=minIndex:
+                minIndex=i
+        if minIndex==0:
+            return True
         return False
