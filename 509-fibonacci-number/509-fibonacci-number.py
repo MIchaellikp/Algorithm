37889fb8fc42cee39140a964f61682c3763d679a
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-13 13:29:19 -0500

    Time: 63 ms (33.78%), Space: 13.8 MB (53.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -8,12 +8,14 @@ class Solution:
         
         
         
-        d = [0] * (n+1)
+        d = [0] * (2)
         
         d[0] = 0
         d[1] = 1
         
         for i in range(2, n+1):
-            d[i] = d[i-1] + d[i-2]
+            temp = d[0] + d[1]
+            d[0] = d[1]
+            d[1] = temp
             
         return d[-1]
