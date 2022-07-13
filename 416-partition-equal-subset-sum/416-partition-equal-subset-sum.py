17cac7ad16cd193f4bc21a566eaa1da4f9be3a61
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-13 17:38:50 -0500

    Time: 217 ms (92.28%), Space: 29.5 MB (35.46%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
         if sum(nums)%2:
@@ -12,4 +12,20 @@ class Solution:
         for i in range(len(nums)):
             for j in range(temp, nums[i] - 1, -1):
                 dp[j] = max(dp[j], dp[j-nums[i]] + nums[i])
-        return dp[temp] == temp 
+        return dp[temp] == temp""" 
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2 == 1:
+            return False
+        target = sum(nums) // 2
+        @lru_cache(maxsize=None)
+        def recursive(n: int, current_sum: int):
+            if current_sum == 0:
+                return True
+            elif current_sum < 0 or n == len(nums) - 1:
+                return False
+            
+            return recursive(n + 1, current_sum - nums[n + 1]) or recursive(n + 1, current_sum)
+        
+        return recursive(0, target)
