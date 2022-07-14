6a5b979875ab16a7edee3be3e9e6637788b43d8a
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-14 18:34:31 -0500

    Time: 46 ms (49.79%), Space: 13.9 MB (11.65%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def climbStairs(self, n: int) -> int:
         dp=[0]*(2)
         dp[0]=1
@@ -7,4 +7,17 @@ class Solution:
             tmp=dp[0]+dp[1]
             dp[0]=dp[1]
             dp[1]=tmp
-        return dp[1]
+        return dp[1]"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0]*(n + 1)
+        dp[0] = 1
+        m = 2
+        # 遍历背包
+        for j in range(n + 1):
+            # 遍历物品
+            for step in range(1, m + 1):
+                if j >= step:
+                    dp[j] += dp[j - step]
+        return dp[n]
