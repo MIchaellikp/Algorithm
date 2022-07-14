772930403b078e3d08c4d43491acddf6cb94cf77
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-14 18:50:44 -0500

    Time: 149 ms (92.72%), Space: 14.9 MB (35.24%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numSquares(self, n: int) -> int:
+    """def numSquares(self, n: int) -> int:
         '''版本二， 先遍历物品, 再遍历背包'''
         # 初始化
         nums = [i**2 for i in range(1, n + 1) if i**2 <= n]
@@ -10,4 +10,14 @@ class Solution:
             # 遍历背包
             for j in range(num, n + 1):
                 dp[j] = min(dp[j], dp[j - num] + 1)
-        return dp[n]
+        return dp[n]"""
+    def numSquares(self, n):
+        squares = [i**2 for i in range(1, int(n**0.5)+1)]
+        d, q, nq = 1, {n}, set()
+        while q:
+            for node in q:
+                for square in squares:
+                    if node == square: return d
+                    if node < square: break
+                    nq.add(node-square)
+            q, nq, d = nq, set(), d+1
