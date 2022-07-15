e7870eb498998b64b77a7f4b4c9acd0443c25a2c
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-15 14:48:02 -0500

    Time: 953 ms (99.33%), Space: 24.9 MB (85.25%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         length = len(prices)
         if len == 0:
@@ -9,4 +9,25 @@ class Solution:
         for i in range(1, length):
             dp[i][0] = max(dp[i-1][0], -prices[i])
             dp[i][1] = max(dp[i-1][1], prices[i] + dp[i-1][0])
-        return dp[-1][1]
+        return dp[-1][1]"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        n=len(prices)
+        
+        max_price=0
+        
+        curr_price=0
+        
+        min_ele=prices[0]
+        
+        for i in range(1,n):
+            curr_price=prices[i]-min_ele
+            
+            if max_price<curr_price:
+                max_price=curr_price
+                
+            if prices[i]<min_ele:
+                min_ele=prices[i]
+                
+        return max_price
