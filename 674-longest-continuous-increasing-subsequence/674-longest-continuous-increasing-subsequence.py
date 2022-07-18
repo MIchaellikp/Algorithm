724ac8a5cca002c2675d3d87cf69252f718e69bd
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-18 15:45:12 -0500

    Time: 75 ms (97.36%), Space: 15.4 MB (9.27%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.py
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.py
@@ -9,8 +9,8 @@ class Solution:
             if nums[i] > nums[i-1]:
                 count += 1
             else:
-                res = max(res, count)
                 count = 1
-                
-        res = max(res, count)
+              
+            res = max(res, count)
+            
         return res
