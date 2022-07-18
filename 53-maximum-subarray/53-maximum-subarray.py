faea8276f728c01f879c075922dfc1347ef86ab1
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-18 16:35:40 -0500

    Time: 1138 ms (43.56%), Space: 28.6 MB (9.82%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    """def maxSubArray(self, nums: List[int]) -> int:
         res = nums[0]
         
         for i in range(1,len(nums)):
@@ -7,4 +7,15 @@ class Solution:
                 nums[i] += nums[i-1]
             res = max(res, nums[i])
             
-        return res
+        return res"""
+    
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        result = dp[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1] + nums[i], nums[i]) #状态转移公式
+            result = max(result, dp[i]) #result 保存dp[i]的最大值
+        return result
