9d8d131c2244d2ba1ab0bbf062ea0b0e4df105ad
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-19 17:01:30 -0500

    Time: 181 ms (65.65%), Space: 14 MB (43.79%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,6 +1,6 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        dp = [[False] * len(s) for _ in range(len(s))]
+        """dp = [[False] * len(s) for _ in range(len(s))]
         result = 0
         for i in range(len(s)-1, -1, -1): #注意遍历顺序
             for j in range(i, len(s)):
@@ -12,4 +12,19 @@ class Solution:
                         result += 1
                         dp[i][j] = True
             print(dp[i])
+        return result"""
+        
+        
+        result = 0
+        for i in range(len(s)):
+            result += self.extend(s, i, i, len(s)) #以i为中心
+            result += self.extend(s, i, i+1, len(s)) #以i和i+1为中心
         return result
+    
+    def extend(self, s, i, j, n):
+        res = 0
+        while i >= 0 and j < n and s[i] == s[j]:
+            i -= 1
+            j += 1
+            res += 1
+        return res
