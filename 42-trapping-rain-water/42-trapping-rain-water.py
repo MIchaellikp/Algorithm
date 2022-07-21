0b2f6e342ec9f4c7dfcf564268cd5583256cc81d
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-21 15:40:29 -0500

    Time: 173 ms (63.39%), Space: 16.1 MB (16.35%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -20,20 +20,39 @@ class Solution:
                 r -= 1
                 
         return res"""
-        lh, rh = [0] * len(height), [0] * len(height) 
+#         lh, rh = [0] * len(height), [0] * len(height) 
         
-        lh[0] = height[0]
+#         lh[0] = height[0]
         
-        for i in range(1, len(height)):
-            lh[i] = max(height[i], lh[i-1])
+#         for i in range(1, len(height)):
+#             lh[i] = max(height[i], lh[i-1])
             
-        rh[-1] = height[-1]
+#         rh[-1] = height[-1]
         
-        for i in range(len(height)-2, -1, -1):
-            rh[i] = max(rh[i+1], height[i])
+#         for i in range(len(height)-2, -1, -1):
+#             rh[i] = max(rh[i+1], height[i])
         
+#         ans = 0
+#         for i in range(len(height)):
+#             ans += min(lh[i], rh[i]) - height[i]
+            
+#         return ans
+        stack = [0]
         ans = 0
-        for i in range(len(height)):
-            ans += min(lh[i], rh[i]) - height[i]
+        for i in range(1, len(height)):
+            if stack and height[i] > height[stack[-1]]:
+                while stack and height[i] > height[stack[-1]]:
+                    stand = height[stack.pop()]
+                    if stack:
+                        l = height[stack[-1]]
+                        r = height[i]
+                        h = min(l,r) - stand
+                        w = i - stack[-1] -1
+                        ans += h*w
+            elif height[i] == height[stack[-1]]:
+                stack.pop()
+            
+            stack.append(i)
             
         return ans
+
