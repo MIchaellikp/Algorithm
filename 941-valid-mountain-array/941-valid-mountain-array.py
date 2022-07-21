fda6a1e0a3d0075b044b346fa855bfcd6e5e005f
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-21 16:32:26 -0500

    Time: 268 ms (67.49%), Space: 15.5 MB (21.56%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -1,18 +1,30 @@
+# class Solution:
+#     def validMountainArray(self, arr: List[int]) -> bool:
+
+#         n = len(arr)
+#         l = 0
+#         r = n - 1
+        
+#         while l < n - 1 and arr[l] < arr[l+1]:
+#                 l += 1
+                
+#         while r > 1 and arr[r] < arr[r - 1]:
+#             r -= 1
+            
+#         return l == r and r != 0 and l != n-1
+            
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-
-        n = len(arr)
-        l = 0
-        r = n - 1
+        length = len(arr)
+        i =0
         
-        while l < n - 1 and arr[l] < arr[l+1]:
-                l += 1
-                
-        while r > 1 and arr[r] < arr[r - 1]:
-            r -= 1
-            
-        return l == r and r != 0 and l != n-1
-            
+        while i+1 < length and arr[i] <arr[i+1]:
+            i+=1
+        if i ==0 or i==len(arr)-1:
+            return False
+        while i+1 <length and arr[i]>arr[i+1]:
+            i+=1
+        return i ==len(arr)-1
         
                 
                 
