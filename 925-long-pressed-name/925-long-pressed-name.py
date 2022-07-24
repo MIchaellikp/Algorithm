2b139ddc681d79a082737d7f3c3473c3fac70fab
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-24 14:41:16 -0500

    Time: 49 ms (50.69%), Space: 13.8 MB (78.98%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.py
+++ 925-long-pressed-name/925-long-pressed-name.py
@@ -1,17 +1,14 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        i = j = 0
-        while(i<len(name) and j<len(typed)):
-        # If the current letter matches, move as far as possible
-            if typed[j]==name[i]:
-                while j+1 < len(typed) and typed[j]==typed[j+1]:
-                    j += 1
-                    # special case when there are consecutive repeating letters
-                    if i+1 < len(name) and name[i]==name[i+1]:
-                        i+=1
-                else:
-                    j+=1
-                    i+=1
-            else:
+        j = 0
+        i = 0
+        n = len(name)
+        t = len(typed)
+        
+        while j < t:
+            if i < n and name[i] == typed[j]:
+                i +=1
+            elif j == 0 or typed[j] != typed[j-1]:
                 return False
-        return i == len(name) and j==len(typed)
+            j +=1
+        return i == n
