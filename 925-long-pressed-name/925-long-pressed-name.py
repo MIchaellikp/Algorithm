f86fb5fc6e15bb1121a7ed7bcf574fef911b6de3
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-24 14:37:50 -0500

    Time: 49 ms (50.69%), Space: 14 MB (33.30%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.py
+++ 925-long-pressed-name/925-long-pressed-name.py
@@ -4,10 +4,10 @@ class Solution:
         while(i<len(name) and j<len(typed)):
         # If the current letter matches, move as far as possible
             if typed[j]==name[i]:
-                while j+1<len(typed) and typed[j]==typed[j+1]:
-                    j+=1
+                while j+1 < len(typed) and typed[j]==typed[j+1]:
+                    j += 1
                     # special case when there are consecutive repeating letters
-                    if i+1<len(name) and name[i]==name[i+1]:
+                    if i+1 < len(name) and name[i]==name[i+1]:
                         i+=1
                 else:
                     j+=1
