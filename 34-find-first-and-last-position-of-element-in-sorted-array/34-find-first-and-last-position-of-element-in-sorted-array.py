02e09384fafdb8d1af8a73b5c41430c521372bbf
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-24 21:05:13 -0500

    Time: 157 ms (25.10%), Space: 15.6 MB (10.25%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,38 +1,37 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def rightsearch (nums:List[int], target:int):
-            l, r = 0, len(nums) - 1
-            ri = -2
-            while (l <= r):
-                m = (l+r)//2
-                if nums[m] > target:
+        
+        
+        def rb(n,r):
+            l, r = 0, len(n) - 1
+            rib = -2
+            while l <= r:
+                m = l + (r - l) //2
+                if n[m] > target:
                     r = m - 1
                 else:
                     l = m + 1
-                    ri = l
-                    
-            return ri
+                    rib = l
+            return rib
         
-        def leftsearch (nums:List[int], target:int):
-            l, r = 0, len(nums) - 1
-            li = -2
-            while (l <= r):
-                m = (l+r)//2
-                if nums[m] < target:
+        def lf(n, r):
+            l, r = 0, len(n) - 1
+            leb = -2
+            while l <= r:
+                m  = l + (r - l) //2
+                if n[m] < target:
                     l = m + 1
                 else:
                     r = m - 1
-                    li = r
-                    
-            return li
-                
-        a = rightsearch (nums, target)
-        b = leftsearch (nums, target)
+                    leb = r
+            return leb
         
         
-        if a == -2 or b == -2: return [-1, -1]
-
-        if a - b > 1:
-            return [b+1, a-1]
+        leb = lf(nums,target)
+        rib = rb(nums, target)
         
-        return [-1, -1]
+        if leb == -2 or rib == -2:
+            return [-1,-1]
+        if rib - leb > 1:
+            return [leb + 1, rib - 1]
+        return [-1,-1]
