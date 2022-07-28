ecd397042e2f4f4fa53d9d9cf29b18887d89417a
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-28 12:54:34 -0500

    Time: 72 ms (43.94%), Space: 14.4 MB (42.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,23 +1,37 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        if not matrix or not matrix[0]:
-            return False
+# class Solution(object):
+#     def searchMatrix(self, matrix, target):
+#         """
+#         :type matrix: List[List[int]]
+#         :type target: int
+#         :rtype: bool
+#         """
+#         if not matrix or not matrix[0]:
+#             return False
 
-        l, r = 0, len(matrix)*len(matrix[0])
+#         l, r = 0, len(matrix)*len(matrix[0])
 
-        while l<r:
-            mid = (l+r)//2
-            i, j = mid//len(matrix[0]), mid%len(matrix[0])
-            if matrix[i][j]==target:
-                return True
-            elif matrix[i][j]>target:
-                r = mid
-            else:
-                l = mid + 1
+#         while l<r:
+#             mid = (l+r)//2
+#             i, j = mid//len(matrix[0]), mid%len(matrix[0])
+#             if matrix[i][j]==target:
+#                 return True
+#             elif matrix[i][j]>target:
+#                 r = mid
+#             else:
+#                 l = mid + 1
     
+#         return False
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row,col = len(matrix)-1, 0
+
+        while 0<=row<len(matrix) and 0<=col<len(matrix[0]):
+                if matrix[row][col] == target:
+                    return True
+                elif matrix[row][col] < target:
+                    col += 1
+                else:
+                    row -= 1
+                    
         return False
