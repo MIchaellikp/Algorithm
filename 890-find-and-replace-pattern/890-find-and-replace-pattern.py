9f8ca6ad51719e189ded6f229d7304d466209089
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-07-29 10:41:04 -0500

    Time: 41 ms (77.16%), Space: 13.8 MB (76.64%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.py
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -1,20 +1,6 @@
 class Solution:
-    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        res = []
-        
-        for i in words:
-            d1, d2 = {}, {}
-            flag = True
-            for j in range(len(i)):
-                if i[j] not in d1:
-                    d1[i[j]] = pattern[j]
-                if pattern[j] not in d2:
-                    d2[pattern[j]] = i[j]
-                    
-                if (d1[i[j]], d2[pattern[j]]) != (pattern[j], i[j]):
-                    flag = False
-                    break
-            if flag:
-                res.append(i)
-                
-        return res
+    def findAndReplacePattern(self, words, p):
+        def F(w):
+            m = {} 
+            return [m.setdefault(c, len(m)) for c in w]
+        return [w for w in words if F(w) == F(p)]
