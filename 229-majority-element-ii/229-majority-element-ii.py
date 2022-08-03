c4689f4365a1f8d0f5c06fb367689431114c8058
MIchaellikp <52867046+MIchaellikp@users.noreply.github.com>
2022-08-02 21:15:25 -0500

    Time: 127 ms (87.56%), Space: 15.3 MB (24.44%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,17 +1,27 @@
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> List[int]:
+#         c = Counter()
+        
+        
+#         for i in nums:
+#             c[i] += 1
+#             if len(c) == 3:
+#                 nc = Counter()
+#                 for j, v in c.items():
+#                     if v != 1:
+#                         nc[j] = v - 1   
+#                 c = nc
+                
+#         pool = Counter ( n for n in nums if n in c)
+        
+#         return [n for n in pool if pool[n] > len(nums)//3]
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        c = Counter()
-        
-        
-        for i in nums:
-            c[i] += 1
-            if len(c) == 3:
-                nc = Counter()
-                for j, v in c.items():
-                    if v != 1:
-                        nc[j] = v - 1   
-                c = nc
-                
-        pool = Counter ( n for n in nums if n in c)
-        
-        return [n for n in pool if pool[n] > len(nums)//3]
+        count=Counter(nums)
+        lst=[]
+        for i in count:
+            if count[i]>(len(nums)//3):
+                lst.append(i)
+        return lst
